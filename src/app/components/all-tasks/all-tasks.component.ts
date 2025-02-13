01a5f8954ad61b054acf981f84448aa54542bac2
Kaique Apolinario <kaiqueapol@outlook.com>
2025-02-13 17:22:30 -0300

    Creating Task and tweaking home page

--- src/app/components/all-tasks/all-tasks.component.ts
+++ src/app/components/all-tasks/all-tasks.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../entity/Task';
 
 @Component({
   selector: 'app-all-tasks',
+  imports: [],
   templateUrl: './all-tasks.component.html',
   styleUrl: './all-tasks.component.scss'
 })
 export class AllTasksComponent {
+
+  taskList: Task[] = [{ "id": 0, "name": "Zero", "finished": false }, {
+    "id": 1, "name": "One", "finished": false
+  }, {
+    "id": 2, "name": "Two~~~~~~~~~~~~~~~~DAAA", "finished": false
+  }, {
+    "id": 3, "name": "Three", "finished": false
+  }, {
+    "id": 4, "name": "Four", "finished": false
+  }, {
+    "id": 5, "name": "Five", "finished": false
+  },{
+    "id": 6, "name": "Six", "finished": false
+  },{
+    "id": 7, "name": "Seven", "finished": false
+  },{
+    "id": 8, "name": "Eight", "finished": false
+  },{
+    "id": 9, "name": "Nine", "finished": false
+  }
+  ];
 
 }
