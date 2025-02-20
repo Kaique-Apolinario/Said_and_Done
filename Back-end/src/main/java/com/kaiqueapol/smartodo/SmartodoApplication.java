975b55c7f3fe3341486c71919e56469bc7d4118f
Kaique Apolinario <kaiqueapol@outlook.com>
2025-02-20 12:50:27 -0300

    Done API documentation with Swagger

--- Back-end/src/main/java/com/kaiqueapol/smartodo/SmartodoApplication.java
+++ Back-end/src/main/java/com/kaiqueapol/smartodo/SmartodoApplication.java
@@ -3,7 +3,11 @@ package com.kaiqueapol.smartodo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title="TaskManagerAPI", version = "1", description = "Documentation of Task Manager API"))
 public class SmartodoApplication {
 
 	public static void main(String[] args) {
--- Back-end/src/main/java/com/kaiqueapol/smartodo/controller/TaskController.java
+++ Back-end/src/main/java/com/kaiqueapol/smartodo/controller/TaskController.java
@@ -16,9 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kaiqueapol.smartodo.entities.Task;
 import com.kaiqueapol.smartodo.services.TaskService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping(value = "/")
+@RequestMapping(value = "/", produces = {"application/json"})
+@Tag(name = "TaskManagerAPI")
 public class TaskController {
 	private TaskService taskService;
 
@@ -27,27 +33,62 @@ public class TaskController {
 	}
 
 	@GetMapping
+	@Operation(summary = "It gets every task in the API", method = "GET")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Sucess"),
+			@ApiResponse(responseCode = "422", description = "Invalid data requisition"),
+			@ApiResponse(responseCode = "400", description = "Invalid parameters"),
+			@ApiResponse(responseCode = "500", description = "Internal server error"),
+	})
 	public ResponseEntity<List<Task>> findAll() {
 		List<Task> list = taskService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
 	@GetMapping(value = "/{id}")
+	@Operation(summary = "It gets a specific task in the API", method = "GET")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Sucess"),
+			@ApiResponse(responseCode = "422", description = "Invalid data requisition"),
+			@ApiResponse(responseCode = "400", description = "Invalid parameters"),
+			@ApiResponse(responseCode = "500", description = "Internal server error"),
+	})
 	public ResponseEntity<Task> findById(@PathVariable Long id) {
 		return ResponseEntity.ok().body(taskService.findById(id));
 	}
 
 	@PostMapping("/save")
+	@Operation(summary = "It saves a task in the API", method = "POST")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Sucess"),
+			@ApiResponse(responseCode = "422", description = "Invalid data requisition"),
+			@ApiResponse(responseCode = "400", description = "Invalid parameters"),
+			@ApiResponse(responseCode = "500", description = "Internal server error"),
+	})
 	public ResponseEntity<Task> save(@RequestBody Task task) {
 		return ResponseEntity.ok(taskService.save(task));
 	}
 
 	@PutMapping("/{id}")
+	@Operation(summary = "It updates a task in the API", method = "PUT")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Sucess"),
+			@ApiResponse(responseCode = "422", description = "Invalid data requisition"),
+			@ApiResponse(responseCode = "400", description = "Invalid parameters"),
+			@ApiResponse(responseCode = "500", description = "Internal server error"),
+	})
 	public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
 		return ResponseEntity.ok().body(taskService.updateTask(id, updatedTask));
 	}
 
-	@DeleteMapping("/{id}")
+	@DeleteMapping("/{id}")	
+	@Operation(summary = "It deletes a task in the API", method = "DELETE")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Sucess"),
+			@ApiResponse(responseCode = "422", description = "Invalid data requisition"),
+			@ApiResponse(responseCode = "400", description = "Invalid parameters"),
+			@ApiResponse(responseCode = "500", description = "Internal server error"),
+	})
 	public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
 		taskService.deleteTask(id);
 		return ResponseEntity.noContent().build();
