8ae2c41be365e2e57c3b7746b7697cb057ed76f3
Kaique Apolinario <kaiqueapol@outlook.com>
2025-02-20 16:13:31 -0300

    Handling possible REST exceptions from Angular requests

--- Front-end/src/app/services/tasks.service.ts
+++ Front-end/src/app/services/tasks.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Task } from '../entity/Task';
-import { Observable } from 'rxjs';
+import { catchError, Observable, retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,22 +11,47 @@ export class TasksService {
   private apiUrl: string = "http://localhost:8080/";
 
   getTasks():Observable<Task[]>{
-    return this.http.get<Task[]>(this.apiUrl);
+    return this.http.get<Task[]>(this.apiUrl).pipe(
+      retry(1),
+     catchError(error => {
+      alert("An error occurred. Sorry! Try reloading the page.")
+      throw error;
+     }));
   } 
 
   getTaskById(taskId: number):Observable<Task> {
-    return this.http.get<Task>(this.apiUrl + taskId);
-  }
+    return this.http.get<Task>(this.apiUrl + taskId).pipe(
+      retry(1),
+     catchError(error => {
+      alert("An error occurred. Sorry! Try reloading the page")
+      throw error;
+     }));
+  } 
 
   addTasks(task:Task):Observable<Task>{
-    return this.http.post<Task>(this.apiUrl + "save", task);
-  }
+    return this.http.post<Task>(this.apiUrl + "save", task).pipe(
+      retry(1),
+     catchError(error => {
+      alert("An error occurred. Sorry! Try reloading the page")
+      throw error;
+     }));
+  } 
 
   deleteTask(task:Task):Observable<Task>{
-    return this.http.delete<Task>(this.apiUrl + task.id);
-  }
+    return this.http.delete<Task>(this.apiUrl + task.id).pipe(
+      retry(1),
+     catchError(error => {
+      alert("An error occurred. Sorry! Try reloading the page")
+      throw error;
+     }));
+  } 
 
   updateTask(oldTask:Task, newTask:Task):Observable<Task>{
-    return this.http.put<Task>(this.apiUrl + oldTask.id, newTask);
-  }
+    return this.http.put<Task>(this.apiUrl + oldTask.id, newTask).pipe(
+      retry(1),
+     catchError(error => {
+      alert("An error occurred. Sorry! Try reloading the page.")
+      throw error;
+     }));
+  } 
 }
